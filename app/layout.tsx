b96f186b0d960e7cb2c164bab0596ffd148515e2
Alessandro b <98426727+AlessandroB1298@users.noreply.github.com>
2024-11-29 09:39:48 -0500

    added public key to vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <ClerkProvider
-            publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-        >
+        <ClerkProvider>
             <html lang="en">
             <body
                 className={`${inter.className} antialiased bg-gradient-to-r from-rose-400 to-red-100`}>

ff9f6b00bbab9c21f713706854a99741b6cefab7
Alessandro b <98426727+AlessandroB1298@users.noreply.github.com>
2024-12-01 19:14:11 -0500

    retry

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -8,6 +8,5 @@ export default function Page() {
             </div>
             <CreateProject/>
         </div>
-
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/app/ui/dashboard/hero"
 
 export default function Home() {
     return (
+
         <main className="flex min-h-screen flex-col p-6">
                 <Hero/>
         </main>

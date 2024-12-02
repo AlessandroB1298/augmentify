035b43c6d21a968bb16171d9e8cf9013fc346851
Alessandro b <98426727+AlessandroB1298@users.noreply.github.com>
2024-12-01 19:19:52 -0500

    retry

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,6 +1,8 @@
 import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
-// import LottieAnimation from "@/app/ui/components/lottieAnimation"
+import LottieAnimation from "@/app/ui/components/lottieAnimation"
+import { Suspense } from 'react';
+
 export default function Page(){
     const user = auth()
     if(!user){
@@ -9,7 +11,10 @@ export default function Page(){
     return(
         <div className={"flex justify-center items-center min-h-screen sm:justify-left font-bold"}>
             <h2 className={"text-white text-[64px] font-solid "}>Welcome to Augmentify</h2>
-            {/*<LottieAnimation/>*/}
+            {/*<LottieAnimation/>*/} {/*need to lazy load this animation*/}
+            <Suspense>
+                <LottieAnimation/>
+            </Suspense>
         </div>
     )
 }

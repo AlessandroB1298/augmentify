b33f4b21383f72b4bb8085c494f46c2e4f150ba6
AlessandroB1298 <aidanbongiorno09@gmail.com>
2025-02-12 03:25:04 -0500

    fixed eslint issues

--- components/dash.tsx
+++ components/dash.tsx
@@ -7,7 +7,6 @@ import { useEffect, useState } from "react";
 import AreaChartPlot from "./ui/areaChart";
 import BarChartPlot from "./ui/barChart";
 
-
 type userData = {
   id?: string;
   name?: string;
@@ -22,7 +21,6 @@ export function Dash() {
   const [data, setData] = useState<userData[]>([]);
   const [projectNum, setProjNum] = useState<number>();
   const [imageTotal, setImageTotal] = useState<number>();
-  const [aiResponse, setAiResponse] = useState<string>("Nothing for Now");
   //trying to set the bar chart data with information from the projects
 
   const retreieveData = async () => {
@@ -53,86 +51,78 @@ export function Dash() {
 
   return (
     <div className="min-h-screen bg-rose-100 p-4 md:p-6 lg:p-8 rounded-xl shadow-xl">
-      <div className="max-w-7xl mx-auto space-y-6">
-        <h1 className="text-2xl md:text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-rose-400 to-pink-500 mb-6">
-          {user?.fullName}'s Dashboard
-        </h1>
+      {isSignedIn && (
+        <div className="max-w-7xl mx-auto space-y-6">
+          <h1 className="text-2xl md:text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-rose-400 to-pink-500 mb-6">
+            {user?.fullName}'s Dashboard
+          </h1>
 
-        <section className="grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-4 gap-4">
-          <div className="bg-white p-4 rounded-xl shadow-md transform duration-75 ease-in hover:bg-gray-200 hover:translate-y-1 hover:cursor-pointer">
-            <h3 className="text-sm font-medium text-gray-500">
-              Number of Projects
-            </h3>
-            <p className="mt-2 text-3xl font-semibold text-gray-900">
-              {projectNum}
-            </p>
-            <p className="mt-1 text-sm font-medium text-rose-400">
-              {user?.fullName}'s Project History
-            </p>
-          </div>
-
-          <div className="bg-white p-4 rounded-xl shadow-md transform duration-75 ease-in hover:bg-gray-200 hover:translate-y-1 hover:cursor-pointer">
-            <h3 className="text-sm font-medium text-gray-500">
-              Recent Project
-            </h3>
-            <p className="mt-2 text-3xl font-semibold text-gray-900">
-              {data[0]?.name}
-            </p>
-            <p className="mt-1 text-sm font-medium text-rose-400">
-              {data?.[0]?.project_type}
-            </p>
-          </div>
-
-          <div className="bg-white p-4 rounded-xl shadow-md transform duration-75 ease-in hover:bg-gray-200 hover:translate-y-1 hover:cursor-pointer">
-            <h3 className="text-sm font-medium text-gray-500">Total Images</h3>
-            <p className="mt-2 text-3xl font-semibold text-gray-900">
-              {imageTotal}
-            </p>
-            <p className="mt-1 text-sm font-medium text-rose-400">
-              Spanning {projectNum} Projects
-            </p>
-          </div>
-        </section>
-
-        {/* Filler code for charts */}
-        <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 ">
-          <div className="bg-white rounded-xl shadow-md p-4 h-80">
-            <h2 className="text-lg font-semibold mb-4">Chart 1</h2>
-            <div className="h-64 bg-gray-50 rounded">
-              <AreaChartPlot />
+          <section className="grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-4 gap-4">
+            <div className="bg-white p-4 rounded-xl shadow-md transform duration-75 ease-in hover:bg-gray-200 hover:translate-y-1 hover:cursor-pointer">
+              <h3 className="text-sm font-medium text-gray-500">
+                Number of Projects
+              </h3>
+              <p className="mt-2 text-3xl font-semibold text-gray-900">
+                {projectNum}
+              </p>
+              <p className="mt-1 text-sm font-medium text-rose-400">
+                {user?.fullName}'s Project History
+              </p>
             </div>
-          </div>
-          <div className="bg-white rounded-xl shadow-md p-4 h-80">
-            <h2 className="text-lg font-semibold mb-4">Chart 2</h2>
-            <div className="h-64 bg-gray-50 rounded">
-              <BarChartPlot />
+
+            <div className="bg-white p-4 rounded-xl shadow-md transform duration-75 ease-in hover:bg-gray-200 hover:translate-y-1 hover:cursor-pointer">
+              <h3 className="text-sm font-medium text-gray-500">
+                Recent Project
+              </h3>
+              <p className="mt-2 text-3xl font-semibold text-gray-900">
+                {data[0]?.name}
+              </p>
+              <p className="mt-1 text-sm font-medium text-rose-400">
+                {data?.[0]?.project_type}
+              </p>
             </div>
-          </div>
-        </section>
 
-        {/* Not sure what to put here yet */}
-
-        <section className="grid grid-cols-1 lg:grid-cols-1 ">
-          <div className="bg-white rounded-xl shadow-md p-4 h-80">
-            <h2 className="text-lg font-semibold mb-4">AI Summary</h2>
-            <div className="h-64 ">
-              <p className="text-rose-300 ">{aiResponse}</p>
+            <div className="bg-white p-4 rounded-xl shadow-md transform duration-75 ease-in hover:bg-gray-200 hover:translate-y-1 hover:cursor-pointer">
+              <h3 className="text-sm font-medium text-gray-500">
+                Total Images
+              </h3>
+              <p className="mt-2 text-3xl font-semibold text-gray-900">
+                {imageTotal}
+              </p>
+              <p className="mt-1 text-sm font-medium text-rose-400">
+                Spanning {projectNum} Projects
+              </p>
             </div>
-          </div>
-        </section>
-      </div>
-    </div>
-  );
-}
+          </section>
 
-function StatCard({ name, project_type, description }: userData) {
-  return (
-    <div className="bg-white p-4 rounded-xl shadow-md transform duration-75 ease-in hover:bg-gray-200 hover:translate-y-1 hover:cursor-pointer">
-      <h3 className="text-sm font-medium text-gray-500">{name}</h3>
-      <p className="mt-2 text-3xl font-semibold text-gray-900">
-        {project_type}
-      </p>
-      <p className="mt-1 text-sm font-medium text-green-600">{description}</p>
+          {/* Filler code for charts */}
+          <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 ">
+            <div className="bg-white rounded-xl shadow-md p-4 h-80">
+              <h2 className="text-lg font-semibold mb-4">Chart 1</h2>
+              <div className="h-64 bg-gray-50 rounded">
+                <AreaChartPlot />
+              </div>
+            </div>
+            <div className="bg-white rounded-xl shadow-md p-4 h-80">
+              <h2 className="text-lg font-semibold mb-4">Chart 2</h2>
+              <div className="h-64 bg-gray-50 rounded">
+                <BarChartPlot />
+              </div>
+            </div>
+          </section>
+
+          {/* Not sure what to put here yet */}
+
+          <section className="grid grid-cols-1 lg:grid-cols-1 ">
+            <div className="bg-white rounded-xl shadow-md p-4 h-80">
+              <h2 className="text-lg font-semibold mb-4">AI Summary</h2>
+              <div className="h-64 ">
+                <p className="text-rose-300 ">Nothing For Now</p>
+              </div>
+            </div>
+          </section>
+        </div>
+      )}
     </div>
   );
 }
